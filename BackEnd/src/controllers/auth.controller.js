611590be993089a9eeef4f92a5912b797b900a61
Sachin <sachinssingh2005@gmail.com>
2025-08-23 22:01:43 +0530

    all authentication routes are done

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js"
 import { generateToken } from "../utils/token.util.js";
 import bcrypt from "bcrypt"
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async(req,res)=>{
     const { email, fullName, password, profilePic } = req.body;
@@ -38,7 +39,6 @@ export const signup = async(req,res)=>{
             })
          }else{
             return res.status(400).json({message: "Something went Wrong"})
-
          }
         
 
@@ -47,10 +47,71 @@ export const signup = async(req,res)=>{
     }
 }
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async(req, res) => {
+  const {email,password} = req.body
+  try {
+    const user = await User.findOne({email}) 
+
+    if(!user){
+        return res.status(400).json({message:"Invalid Credentials"})
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password , user.password)
+
+    if(!isPasswordCorrect){
+                return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    generateToken(user._id,res)
+    res.status(200).json({user:{
+        ...user._doc,
+        password:null
+    }})
+  } catch (error) {
+    console.log(`Internal Error ${error}`)
+    res.status(500).json({message:`Internal Server Error`})
+  }
 };
 
-export const logout = (req, res) => {
-  res.send("logout");
+export const logout = async(req, res) => {
+    try {
+        await res.cookie("jwt_token", "",{maxAge:0});
+        res.status(200).json({message:"Successfully logout"})
+    } catch (error) {
+        console.log("Internal Server Error")
+    }
 };
+
+export const updateProfile = async(req,res)=>{
+    try {
+        const {profilePic} = req.body
+        const userID = req.user._id
+
+        if(!profilePic){
+                        res
+                          .status(400)
+                          .json({ message: "Profile Picture is required" });
+            
+            const uploadRes = await cloudinary.uploader.upload(profilePic)
+            const updatedUser = await User.findById(userID,{profilePic:uploadRes.secure_url},{new:true})
+
+                        res
+                          .status(200)
+                          .json({updatedUser});
+
+        }
+    } catch (error) {
+        console.log(`Error in updating the profile ${error}`)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json({success:true , user:req.user})
+    } catch (error) {
+        console.log("Error in checkAuth controller ", error.message)
+        res.status(500).json({message:"Internal Server Error"})
+
+    }
+}
---
+++ BackEnd/src/middleware/protectRoute.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+import User from "../model/user.model.js"
+
+export const protectRoute = async(req,res,next)=>{
+    try {
+        const token = req.cookies.jwt_token;
+        if(!token){
+           return res.status(401).json({message:"Unauthorized Access"})
+        }
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        if(!decoded){
+            return res.status(401).json({ message: "Unauthorized Access" });
+        }
+        const user = await User.findById(decoded.userID).select("-password")
+
+        if(!user){
+                        return res
+                          .status(404)
+                          .json({ message: "User not found" });
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        console.log(`Error in protected route Middleware ${error}`)
+        res.status(500).json({message:"Internal Server error"})
+    }
+}
